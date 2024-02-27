5f5359aabb6f68a5760c286c0b7b91cb83ab194d
tync0 <isgenderlituncay2004@gmail.com>
2024-02-27 20:17:45 +0400

    piton

--- models/base_model.py
+++ models/base_model.py
@@ -1,25 +1,19 @@
-#!/usr/bin/python3
 """Defines a class called BaseModel"""
+
 import uuid
 from datetime import datetime
 
 
 class BaseModel:
     def __init__(self, *args, **kwargs):
-        if kwargs is not None:
-            for key, value in kwargs.items():
-                if key == "created_at" or key == "updated_at":
-                    setattr(
-                        self,
-                        key,
-                        datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"),
-                    )
-                elif key != "__class__":
-                    setattr(self, key, value)
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+        for key, value in kwargs.items():
+            if key == "created_at" or key == "updated_at":
+                setattr(self, key, datetime.fromisoformat(value))
+            elif key != "__class__":
+                setattr(self, key, value)
 
     def save(self):
         self.updated_at = datetime.now()
@@ -37,3 +31,29 @@ class BaseModel:
         dictionary["created_at"] = self.created_at.isoformat()
         dictionary["updated_at"] = self.updated_at.isoformat()
         return dictionary
+
+
+if __name__ == "__main__":
+    my_model = BaseModel()
+    my_model.name = "My_First_Model"
+    my_model.my_number = 89
+    print(my_model.id)
+    print(my_model)
+    print(type(my_model.created_at))
+    print("--")
+    my_model_json = my_model.to_dict()
+    print(my_model_json)
+    print("JSON of my_model:")
+    for key in my_model_json.keys():
+        print(
+            "\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key])
+        )
+
+    print("--")
+    my_new_model = BaseModel(**my_model_json)
+    print(my_new_model.id)
+    print(my_new_model)
+    print(type(my_new_model.created_at))
+
+    print("--")
+    print(my_model is my_new_model)
