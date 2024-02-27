f7a62bfb36abc7cede9d6fc0dfbb1d0de20cab70
tync0 <isgenderlituncay2004@gmail.com>
2024-02-27 20:05:20 +0400

    gg

--- models/base_model.py
+++ models/base_model.py
@@ -9,7 +9,11 @@ class BaseModel:
         if kwargs is not None:
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
-                    setattr(self, key, datetime.fromisoformat(value))
+                    setattr(
+                        self,
+                        key,
+                        datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"),
+                    )
                 elif key != "__class__":
                     setattr(self, key, value)
         else:
