58d7883221e0802797d658468a4ec2aadddd1744
tync0 <isgenderlituncay2004@gmail.com>
2024-02-27 19:50:52 +0400

    C > Python

--- models/base_model.py
+++ models/base_model.py
@@ -1,25 +1,26 @@
 #!/usr/bin/python3
+"""Defines a class called BaseModel"""
 import uuid
-import datetime
+from datetime import datetime
 
 
 class BaseModel:
     def __init__(self, *args, **kwargs):
         if kwargs is not None:
             for key, value in kwargs.items():
-                if key == "id":
-                    setattr(self, key, value)
+                if key == "created_at" or key == "updated_at":
+                    setattr(self, key, datetime.fromisoformat(value))
                 elif key == "__class__":
                     continue
                 else:
-                    setattr(self, key, datetime.datetime.fromisoformat(value))
+                    setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.datetime.now()
-            self.updated_at = datetime.datetime.now()
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def save(self):
-        self.updated_at = datetime.datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
         return "[{}] ({}) {}".format(
