d460eb160575d62e95d6bbfc818bc3487ec653a6
tync0 <isgenderlituncay2004@gmail.com>
2024-02-27 18:57:28 +0400

    Creating BaseClass

---
+++ models/base_model.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+import uuid
+import datetime
+
+
+class BaseModel:
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
+
+    def save(self):
+        self.updated_at = datetime.datetime.now()
+
+    def __str__(self):
+        return "[{}] ({}) {}".format(
+            self.__class__.__name__,
+            self.id,
+            self.__dict__,
+        )
+
+    def to_dict(self):
+        dictionary = self.__dict__.copy()
+        dictionary["__class__"] = self.__class__.__name__
+        dictionary["created_at"] = self.created_at.isoformat()
+        dictionary["updated_at"] = self.updated_at.isoformat()
+        return dictionary
