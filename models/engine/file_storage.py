78129ab44e77068aa0ae9c87f16c6d9a4c2ced03
tync0 <isgenderlituncay2004@gmail.com>
2024-02-28 14:23:49 +0400

    try except

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -16,11 +16,13 @@ class FileStorage:
             json.dump(FileStorage.__objects, f)
 
     def reload(self):
-        if path.exists(FileStorage.__file_path):
+        try:
             with open(FileStorage.__file_path, "r") as f:
                 data = json.load(f)
             for key in data:
                 FileStorage.__objects[key] = BaseModel(**data[key])
+        except Exception:
+            pass
 
     def new(self, obj):
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
