3cfab1b3a4295fb24cf934f83dbd886539dfffb5
tync0 <isgenderlituncay2004@gmail.com>
2024-02-28 14:21:57 +0400

    try5

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -18,7 +18,9 @@ class FileStorage:
     def reload(self):
         if path.exists(FileStorage.__file_path):
             with open(FileStorage.__file_path, "r") as f:
-                FileStorage.__objects = json.load(f)
+                data = json.load(f)
+            for key in data:
+                FileStorage.__objects[key] = BaseModel(**data[key])
 
     def new(self, obj):
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
