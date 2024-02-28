9c03b3eb3ee6c446ef8189d62f944573bd7ec9f4
tync0 <isgenderlituncay2004@gmail.com>
2024-02-28 13:41:45 +0400

    File Storage

--- models/base_model.py
+++ models/base_model.py
@@ -2,21 +2,26 @@
 
 import uuid
 from datetime import datetime
+import models
 
 
 class BaseModel:
     def __init__(self, *args, **kwargs):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-        for key, value in kwargs.items():
-            if key == "created_at" or key == "updated_at":
-                setattr(self, key, datetime.fromisoformat(value))
-            elif key != "__class__":
-                setattr(self, key, value)
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    setattr(self, key, datetime.fromisoformat(value))
+                elif key != "__class__":
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def save(self):
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def __str__(self):
         return "[{}] ({}) {}".format(
@@ -31,29 +36,3 @@ class BaseModel:
         dictionary["created_at"] = self.created_at.isoformat()
         dictionary["updated_at"] = self.updated_at.isoformat()
         return dictionary
-
-
-if __name__ == "__main__":
-    my_model = BaseModel()
-    my_model.name = "My_First_Model"
-    my_model.my_number = 89
-    print(my_model.id)
-    print(my_model)
-    print(type(my_model.created_at))
-    print("--")
-    my_model_json = my_model.to_dict()
-    print(my_model_json)
-    print("JSON of my_model:")
-    for key in my_model_json.keys():
-        print(
-            "\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key])
-        )
-
-    print("--")
-    my_new_model = BaseModel(**my_model_json)
-    print(my_new_model.id)
-    print(my_new_model)
-    print(type(my_new_model.created_at))
-
-    print("--")
-    print(my_model is my_new_model)
---
+++ models/engine/file_storage.py
@@ -0,0 +1,25 @@
+import json
+import os.path as path
+from models.base_model import BaseModel
+
+
+class FileStorage:
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        return self.__objects
+
+    def save(self):
+        with open(FileStorage.__file_path, "w") as f:
+            json.dump(f, FileStorage.__objects)
+
+    def reload(self):
+        file = FileStorage.__file_path
+        if path.exists(file):
+            with open(file, "r") as f:
+                FileStorage.__objects = json.dump(f)
+
+    def new(self, obj):
+        key = "{}.{}".format(obj.__class__.name, obj.id)
+        FileStorage.__objects[key] = obj.to_dict()
