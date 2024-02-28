01e4e1e030fb055e7201004d9cf59ea90c931ab7
tync0 <isgenderlituncay2004@gmail.com>
2024-02-28 19:22:45 +0400

    try8

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -13,7 +13,8 @@ class FileStorage:
     def save(self):
         data = {}
         for key, value in FileStorage.__objects.items():
-            data[key] = value
+            data[key] = value.to_dict()
+
         with open(FileStorage.__file_path, "w") as f:
             json.dump(data, f)
 
@@ -28,4 +29,4 @@ class FileStorage:
 
     def new(self, obj):
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
-        FileStorage.__objects[key] = obj.to_dict()
+        FileStorage.__objects[key] = obj
