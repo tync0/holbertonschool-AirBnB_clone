50657ce368d800c7596f5b6adf1909d48797ce3e
tync0 <isgenderlituncay2004@gmail.com>
2024-02-28 13:43:02 +0400

    add comment

--- models/base_model.py
+++ models/base_model.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 """Defines a class called BaseModel"""
 
 import uuid
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 import json
 import os.path as path
 from models.base_model import BaseModel
