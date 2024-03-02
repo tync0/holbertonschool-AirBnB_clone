3e88a1a03866d8c219a61e7da2e1e70208cdfacd
Elnur <elnurra@code.edu.az>
2024-03-02 20:04:39 +0000

    feat: added test for user

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,5 +1,5 @@
-#!/usr/bin/python
-""" holds class Amenity"""
+#!/usr/bin/python3
+""" holds class User"""
 import models
 from models.base_model import BaseModel, Base
 from os import getenv
@@ -8,14 +8,22 @@ from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 
-class Amenity(BaseModel, Base):
-    """Representation of Amenity """
+class User(BaseModel, Base):
+    """Representation of a user """
     if models.storage_t == 'db':
-        __tablename__ = 'amenities'
-        name = Column(String(128), nullable=False)
+        __tablename__ = 'users'
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+        places = relationship("Place", backref="user")
+        reviews = relationship("Review", backref="user")
     else:
-        name = ""
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
 
     def __init__(self, *args, **kwargs):
-        """initializes Amenity"""
+        """initializes user"""
         super().__init__(*args, **kwargs)
