b2fecbb0918955226aab190dfb0d5e8d924d24f0
Elnur <elnurra@code.edu.az>
2024-03-02 21:43:42 +0000

    feat: test amenity changed

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,40 +1,31 @@
 #!/usr/bin/python3
+
+import os
 import unittest
+
+from models import storage
 from models.amenity import Amenity
-"""
-Unittest Module for Amenity class
-"""
 
 
 class TestAmenity(unittest.TestCase):
-    ''' Unittest for Amenity class '''
+    """
+    This class contains unit tests for the Amenity class.
+    """
 
-    def test_object_Instantiation(self):
-        ''' instantiates class '''
-        self.amenity = Amenity()
+    def setUp(self):
+        try:
+            os.remove("file.json")
+        except IOError:
+            pass
 
-    def testattr(self):
-        ''' test Class: Amenity attributes '''
-        self.amenity = Amenity()
-        self.assertTrue(hasattr(self.amenity, "created_at"))
-        self.assertTrue(hasattr(self.amenity, "updated_at"))
-        self.assertFalse(hasattr(self.amenity, "random_attr"))
-        self.assertTrue(hasattr(self.amenity, "name"))
-        self.assertTrue(hasattr(self.amenity, "id"))
-        self.assertEqual(self.amenity.__class__.__name__, "Amenity")
+        storage.__objects = {}
 
-    def testsave(self):
-        ''' testing method: save '''
-        self.amenity = Amenity()
-        self.amenity.save()
-        self.assertTrue(hasattr(self.amenity, "updated_at"))
+    def test_check_type(self):
+        self.assertIsInstance(Amenity.name, str)
 
-    def teststr(self):
-        ''' testing __str__ return format of Amenity '''
-        self.amenity = Amenity()
-        s = "[{}] ({}) {}".format(self.amenity.__class__.__name__,
-                                  str(self.amenity.id), self.amenity.__dict__)
-        self.assertEqual(print(s), print(self.amenity))
+    def test_name(self):
+        amenity_model = Amenity()
+        amenity_model.name = "Test"
+        self.assertEqual(amenity_model.name, "Test")
 
-if __name__ == '__main__':
-    unittest.main()
+# test_amenity.py
