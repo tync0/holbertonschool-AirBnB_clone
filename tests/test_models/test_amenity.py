986e7c9513942487fc021039d6dc52636d75e0a7
Elnur <elnurra@code.edu.az>
2024-03-02 21:15:00 +0000

    test: fpr amenity fixed

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,29 +1,40 @@
 #!/usr/bin/python3
-""" holds class User"""
-import models
-from models.base_model import BaseModel, Base
-from os import getenv
-import sqlalchemy
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
+import unittest
+from models.amenity import Amenity
+"""
+Unittest Module for Amenity class
+"""
 
 
-class User(BaseModel, Base):
-    """Representation of a user """
-    if models.storage_t == 'db':
-        __tablename__ = 'users'
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=True)
-        last_name = Column(String(128), nullable=True)
-        places = relationship("Place", backref="user")
-        reviews = relationship("Review", backref="user")
-    else:
-        email = ""
-        password = ""
-        first_name = ""
-        last_name = ""
+class TestAmenity(unittest.TestCase):
+    ''' Unittest for Amenity class '''
 
-    def __init__(self, *args, **kwargs):
-        """initializes user"""
-        super().__init__(*args, **kwargs)
+    def test_object_Instantiation(self):
+        ''' instantiates class '''
+        self.amenity = Amenity()
+
+    def testattr(self):
+        ''' test Class: Amenity attributes '''
+        self.amenity = Amenity()
+        self.assertTrue(hasattr(self.amenity, "created_at"))
+        self.assertTrue(hasattr(self.amenity, "updated_at"))
+        self.assertFalse(hasattr(self.amenity, "random_attr"))
+        self.assertTrue(hasattr(self.amenity, "name"))
+        self.assertTrue(hasattr(self.amenity, "id"))
+        self.assertEqual(self.amenity.__class__.__name__, "Amenity")
+
+    def testsave(self):
+        ''' testing method: save '''
+        self.amenity = Amenity()
+        self.amenity.save()
+        self.assertTrue(hasattr(self.amenity, "updated_at"))
+
+    def teststr(self):
+        ''' testing __str__ return format of Amenity '''
+        self.amenity = Amenity()
+        s = "[{}] ({}) {}".format(self.amenity.__class__.__name__,
+                                  str(self.amenity.id), self.amenity.__dict__)
+        self.assertEqual(print(s), print(self.amenity))
+
+if __name__ == '__main__':
+    unittest.main()
