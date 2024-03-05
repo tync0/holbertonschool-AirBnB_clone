b9572ef7c2382f940ba42399e4e0716162b1598a
tync0 <isgenderlituncay2004@gmail.com>
2024-03-05 14:48:27 +0400

    gr

--- console.py
+++ console.py
@@ -3,6 +3,7 @@
 This module contains the entry point of the command interpreter.
 """
 import cmd
+from models.base_model import BaseModel
 
 
 class HBNBCommand(cmd.Cmd):
@@ -30,6 +31,30 @@ class HBNBCommand(cmd.Cmd):
         Do nothing when an empty line is entered.
         """
         pass
+
+    def do_create(self, args):
+        """
+        Create an instance of BaseModel
+        """
+        if len(args) == 0:
+            print("** class name missing **")
+        elif args[0] != "BaseModel":
+            print("** class doesn't exist **")
+        elif len(args) == 1 and args[0] == "BaseModel":
+            new_inst = BaseModel()
+            new_inst.save()
+
+    def do_show(self, args):
+        """
+        Show an instance of BaseModel
+        """
+        print(args[0] == "BaseModel")
+        if len(args) == 0:
+            print("** class name missing **")
+        elif args[0] != "BaseModel":
+            print("** class doesn't exist **")
+        elif len(args) == 1:
+            print("** instance id missing **")
 
 
 if __name__ == "__main__":
